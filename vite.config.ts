84ab5ba92b066faec4cd1f6d8a6d7f2763a91fbf
Alisson Ayala <alisson.ayala2025@poma.superate.org.sv>
2025-11-01 18:19:30 -0600

    Subiendo build de GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/alissonayalae.github.io/', 
+  base: '/', 
   plugins: [
     vue(),
     vueDevTools(),
